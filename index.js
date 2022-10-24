fb9d6508b7d4f22f1830259f25cc91ec17dc6576
omolaso <adextee001@gmail.com>
2022-10-24 05:20:40 +0100

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 //to confirm that dotenv is working
 // console.log(process.env.CLIENT_ID)
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
 
 const express = require('express');
 
@@ -8,8 +12,9 @@ const app = express();
 
 //EXPRESS BASIC SYNTAX
 //app.METHOD(PATH, HANDLER[note: handler is the callback function that runs everytime a user hits a specific URL])
-app.get('/', (req, res) => {
-    res.send('Hey World');
+app.get('/login', (req, res) => {
+    res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&
+    redirect_uri=${REDIRECT_URI}`);
 });
 
 const port = 8888;
