06f90d49b0ea758d90e03ca7572f5ed39d728123
omolaso <adextee001@gmail.com>
2022-11-07 05:13:32 +0100

    added to the single playlist file

--- client/src/components/SinglePlaylist.jsx
+++ client/src/components/SinglePlaylist.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { catchErrors } from '../utils'
+import { getPlaylistById } from '../spotify';
+import { StyledHeader } from '../styles';
+
 
 const SinglePlaylist = () => {
+
+  const { id } = useParams();
+  const [singlePlaylist, setSinglePlaylist] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await getPlaylistById(id);
+      setSinglePlaylist(data);
+    };
+
+    catchErrors(fetchData());
+  }, [id]);
+
+  // console.log(singlePlaylist);
+
   return (
-    <div>
-      <h1>Single Playlist</h1>
-    </div>
+    <>
+    {singlePlaylist && (
+      <>
+        <StyledHeader>
+          <div className="header__inner">
+            {singlePlaylist.images.length && singlePlaylist.images[0].url && (
+              <img className="header__img" src={singlePlaylist.images[0].url} alt="singlePlaylist Artwork"/>
+            )}
+            <div>
+              <div className="header__overline">Playlist</div>
+              <h1 className="header__name">{singlePlaylist.name}</h1>
+              <p className="header__meta">
+                {singlePlaylist.followers.total ? (
+                  <span>{singlePlaylist.followers.total} {`follower${singlePlaylist.followers.total !== 1 ? 's' : ''}`}</span>
+                ) : null}
+                <span>{singlePlaylist.tracks.total} {`song${singlePlaylist.tracks.total !== 1 ? 's' : ''}`}</span>
+              </p>
+            </div>
+          </div>
+        </StyledHeader>
+      </>
+    )}
+  </>
   )
+
 }
 
 export default SinglePlaylist
